c469d4fa135c987cf04f95115db1320a80100dd8
Your Name <213172607@mycput.ac.za>
2021-04-01 16:02:10 +0200

    Disable

---
+++ src/test/java/za/ac/cput/Disabling.java
@@ -0,0 +1,17 @@
+package za.ac.cput;
+
+public class Disabling {
+
+
+
+        public void theMethod(){
+
+            String a = "This method";
+        }
+        public void the  Method2(){
+
+            String b = "This method 2";
+
+
+    }
+}
